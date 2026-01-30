a97e436e337e0542cffd9233b5b1a8fc331872a8
TmohamedAli <tammammohamedali@gmail.com>
2026-01-30 17:04:52 +0000

    Wrap user prompt in messages list with role=user
    Use Content messages instead of raw string for Gemini request

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 def main():
-    #---Initialise-Gemini---
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
+    api_key = os.environ.get("GEMINI_API_KEY")#Read the API key from .env so we don't hard-code secrets
     if api_key == None:
         raise RuntimeError("Api key not found")
-
     client = genai.Client(api_key=api_key)
 
-    #---User-input-parser---
+    
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    #---Gemini-prompting---
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents= args.user_prompt
+    model='gemini-2.5-flash', contents= messages
     )
     if response.usage_metadata == None:
         raise RuntimeError("Failed api request")
