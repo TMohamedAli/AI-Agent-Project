cce414ddd16d1c7a8bb7bf8ca3165b238938cea3
TmohamedAli <tammammohamedali@gmail.com>
2026-01-31 13:58:00 +0000

    Add --Verbose flag for developer debugging and cleaner user experience

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 import os
+import argparse
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-import argparse
+
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")#Read the API key from .env so we don't hard-code secrets
     if api_key == None:
-        raise RuntimeError("Api key not found")
-    client = genai.Client(api_key=api_key)
-
+        raise RuntimeError("GEMINI_API_KEY enviroment variable not set")
     
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    args = parser.parse_args()
+    client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages, args.verbose, args.user_prompt)
 
+
+def generate_content(client, messages, verbose, user_prompt):   
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents= messages
+        model='gemini-2.5-flash', 
+        contents= messages
     )
     if response.usage_metadata == None:
-        raise RuntimeError("Failed api request")
+        raise RuntimeError("Gemini API response appears to be malformed")
     
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)     
+    print("Response:")
     print(response.text)
 
 
