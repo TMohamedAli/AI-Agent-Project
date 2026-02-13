e6e27268ff18d60d33754563301bdef8d39f25fd
TmohamedAli <tammammohamedali@gmail.com>
2026-02-13 13:57:34 +0000

    feat: Implement call_function and map tool responses to the agent

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 MAX_CHARACTERS = 10000
 MODEL_NAME = 'gemini-2.5-flash'
+WORKING_DIR = "./calculator"
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google import genai
 from google.genai import types
 from prompts import system_prompt
 from config import MODEL_NAME
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
@@ -49,8 +49,18 @@ def generate_content(client, messages, verbose):
         print(response.text)
         return
     
+    function_results = []
     for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")         
+        result = call_function(function_call, verbose)
+        if (
+            not result.parts
+            or not result.parts[0].function_response
+            or not result.parts[0].function_response.response
+        ):
+            raise RuntimeError(f"Empty fucntion resposne for {function_call.name}")
+        if verbose: 
+            print(f"-> {result.parts[0].function_response.response}")
+        function_results.append(result.parts[0])
 
 if __name__ == "__main__":
     main()
